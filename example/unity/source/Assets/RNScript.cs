38b769fe71b7a3faec08ff6639865e17f38c60b9
azesm <azesm@me.com>
2022-04-29 13:02:23 +0300

    unity source

---
+++ example/unity/source/Assets/RNScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RNScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MessageRN(string message)
+    {
+        print("UNITY Recived message: " + message);
+    }
+}
